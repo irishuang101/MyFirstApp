1333c215ac5a6b4a98a7a2e80de22670e1f0b6e5
irishuang101 <26huangi49@stu.smuhsd.org>
2023-07-12 14:35:16 -0700

    Initial Commit

---
+++ MyFirstApp/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  MyFirstApp
+//
+//  Created by Scholar on 7/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Text("Lassen National Park")
+                .font(.title)
+                .fontWeight(.regular)
+                .foregroundColor(Color.blue)
+            Image(resizedImage2)
+
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
